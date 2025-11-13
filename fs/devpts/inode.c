2b9b01c26fac72e453b6e0867a9e65f49ef4a22b
Rama Bondan Prakoso <ramanarubp@gmail.com>
2025-11-13 23:52:30 +0700

    Revert "Kernel: Fix KernelSU manual hook"
    this is useless
    
    This reverts commit 2e84fee68321732d369a46271a2437941e2e123b.

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -605,9 +605,6 @@ extern int ksu_handle_devpts(struct inode*);
 
 #if defined(CONFIG_KSU_SUSFS_SUS_SU)
 extern bool ksu_devpts_hook;
-#endif
-
-#ifdef CONFIG_KSU_MANUAL_HOOK
 extern int ksu_handle_devpts(struct inode*);
 #endif
 
